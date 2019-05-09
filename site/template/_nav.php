3754f06f47bf13231d20295de6d4f5fbf4e8aad8
webew <fillion.bertrand@gmail.com>
2019-05-09 23:50:04 +0200

    first commit

--- site/template/_nav.php
+++ site/template/_nav.php
@@ -1,6 +1,6 @@
 <nav>
     <ul>
-        <li><a href="admin/index.php">Admin</a></li>
+        <li><a href="../admin/index.php">Admin</a></li>
         <li><a href="index.php">Accueil</a></li>
         <li><a href="articles.php">Articles</a></li>
     </ul>
